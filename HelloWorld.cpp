b16f1446527d78d6252d895253bc42dc323b569b
Jalynn Nicoly <97772154+Jalynnn@users.noreply.github.com>
2026-01-14 13:48:11 -0700

    ClassDemo

---
+++ HelloWorld.cpp
@@ -0,0 +1,26 @@
+// input output stream
+// C++ library
+// more files = more frequent
+#include <iostream>
+
+// standard = std
+// avoid naming conflicts
+using namespace std;
+
+// function {}
+// int = integer 
+// variable return type
+// main function = the name of this function is main
+// main always returns an int
+// main never has parameters
+int main(/* Parameters */) {
+    // purpose of main = root of your code
+    // cout = console output
+    // "Hello World!" = a string
+    // \n = new line
+cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
+
+    // return line is optional
+    return 0;
+}
